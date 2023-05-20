66ea3767f72316571a6153ccaa4592aa9b5a6195
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-05-20 21:20:27 +0545

    Add: Main python file

---
+++ main.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import bs4 as bs
+import urllib.request
+import pickle
+import requests
+
+# load the nlp model and tfidf vectorizer from disk
+filename = 'nlp_model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+vectorizer = pickle.load(open('tranform.pkl','rb'))
+
+def create_similarity():
+    data = pd.read_csv('main_data.csv')
+    # creating a count matrix
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    similarity = cosine_similarity(count_matrix)
+    return data,similarity
+
+
+def get_suggestions():
+    data = pd.read_csv('main_data.csv')
+    return list(data['movie_title'].str.capitalize())
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    suggestions = get_suggestions()
+    return render_template('home.html',suggestions=suggestions)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
